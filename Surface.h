803b96e7581efa4353fc87cb7555a52b5ce59757
pitkajuh <github_pj@protonmail.com>
2024-11-16 13:06:33 +0200

    Ok

--- Surface.h
+++ Surface.h
@@ -22,26 +22,31 @@ public:
   double M=0;
   double K=0;
   virtual const double SurfaceEquation (const Coordinate &p)=0;
-  void DistanceSet()
+  virtual const double SurfaceDistance(const Coordinate &positionAt, const Coordinate &directionTo)=0;
+  const double Distance(const Coordinate &positionAt, const Coordinate &directionTo)
   {
-    // K=Ax2 + By2 + Cz2 + Dxy + Eyz + F xz + Gx + Hy + Iz + J;
-    // L=2(Aux + Bvy + Cwz) + D(vx + uy) + E(wy + vz) + F (wx + uz)+ Gu + Hv + Iw;
-    // M=Au2 + Bv2 + Cw2 + Duv + Evw + F uw;
-  }
-  const double Distance(const Coordinate &p)
-  {
-    // const double L=0;
-    // const double M=0;
-    // const double K=0;
+    const double u=directionTo.x;
+    const double v=directionTo.y;
+    const double w=directionTo.z;
+    const double x=positionAt.x;
+    const double y=positionAt.y;
+    const double z=positionAt.z;
+    K=A*x*x+B*y*y+C*z*z+D*x*y+E*y*z+F*x*z+G*x+H*y+I*z+J;
+    L=2*(A*u*x+B*v*y+C*w*z)+D*(v*x+u*y)+E*(w*y+v*z)+F*(w*x+u*z)+G*u+H*v+I*w;
+    M=A*u*u+B*v*v+C*w*w+D*u*v+E*v*w+F*u*w;
     const double inSqrt=L*L-4*M*K;
-    // Check if inSqrt negative.
+
+    // No solution exists, the surface is away from line-of-sight.
+    if(inSqrt<0) return 0;
+
     const double sqrtValue=pow(inSqrt, 0.5);
     const double optionPositive=-L+sqrtValue;
-    // const double optionNegative=-L-sqrtValue;
-    // Check which option is valid
-    const double result=optionPositive;
-    return result/(2*M);
+    const double optionNegative=-L-sqrtValue;
 
+    // No solution exists, the surface is away from line-of-sight.
+    if(optionPositive<0 and optionNegative<0) return 0;
+    else if(optionPositive>0 and optionNegative<0) return optionPositive/(2*M);
+    else return optionNegative/(2*M);
   }
   const bool SurfaceTest (const Coordinate &p)
   {
@@ -64,28 +69,59 @@ public:
     B=1;
     J=-1;
   }
+  const double SurfaceDistance(const Coordinate &positionAt, const Coordinate &directionTo)
+  {
+    return 0;
+  }
   const double SurfaceEquation (const Coordinate &p){return (p.y-centeredAt.y)*(p.y-centeredAt.y)+(p.x-centeredAt.x)*(p.x-centeredAt.x)-value*value;}
 };
 
 class PlaneX: public Surface
 {
 public:
-  PlaneX (const double v){value=v;}
+  PlaneX (const double v)
+  {
+    value=v;
+    G=1;
+    J=1;
+  }
   const double SurfaceEquation (const Coordinate &p){return p.x-value;}
+  const double SurfaceDistance(const Coordinate &positionAt, const Coordinate &directionTo)
+  {
+    return 0;
+  }
 };
 
 class PlaneY: public Surface
 {
 public:
-  PlaneY (const double v){value=v;}
+  PlaneY (const double v)
+  {
+    value=v;
+    H=1;
+    J=1;
+  }
   const double SurfaceEquation (const Coordinate &p){return p.y-value;}
+  const double SurfaceDistance(const Coordinate &positionAt, const Coordinate &directionTo)
+  {
+    return 0;
+  }
 };
 
 class PlaneZ: public Surface
 {
 public:
-  PlaneZ (const double v){value=v;}
+  PlaneZ (const double v)
+  {
+    value=v;
+    I=1;
+    J=1;
+  }
   const double SurfaceEquation (const Coordinate &p){return p.z-value;}
+  const double SurfaceDistance(const Coordinate &positionAt, const Coordinate &directionTo)
+  {
+    return 0;
+  }
 };
 
 #endif
