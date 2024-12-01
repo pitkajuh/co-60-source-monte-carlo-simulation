2c1928ab9fcacd29a1a10c346021c3c6eb74bc19
pitkajuh <github_pj@protonmail.com>
2024-12-01 17:26:46 +0200

    Ok

--- ReadCrossSections.cpp
+++ ReadCrossSections.cpp
@@ -13,44 +13,55 @@ using std::streampos;
 using std::map;
 using std::stod;
 
+void print(const vector<double> &v)
+{
+  string s="";
+  for(const auto &i: v){s=s+";"+std::to_string(i);}
+  cout<<s<<" SIZE "<<v.size()<<'\n';
+}
+
+void FillVector(vector<double> &v){while(v.size()<6) v.push_back(0);}
+
 void LineSplit(string line, vector<double> &v)
 {
   const int at=distance(line.begin(), find(line.begin(), line.end(), ' '));
   const string lineSplit=line.substr(0, at);
   const string line2=line.substr(at+1, line.size());
   const int size=line2.size();
-  cout<<lineSplit<<";"<<line2<<" "<<at<<" "<<size<<'\n';
 
-  v.push_back(stod(lineSplit));
+  if(at==0) FillVector(v);
+  else v.push_back(stod(lineSplit));
 
   if(at!=size) LineSplit(line2, v);
-  else
-    {
-      v.push_back(stod(line2));
-      // cout<<v.size()<<'\n';
-      cout<<line2<<'\n';
-      cout<<" "<<'\n';
-      v.clear();
-      // Add vector to map
-    }
+  else if(size>0) v.push_back(stod(line2));
 }
 
 void ReadENDF(ifstream &endf, streampos *&from, const string &id)
 {
   string line;
-  bool stop=false;
+  string line2;
+  bool found=false;
   vector<double> v;
   int linesSkip=3;
+  int size;
+  const int idSize=id.size();
 
-  while(getline(endf, line) and !stop)
+  while(getline(endf, line))
     {
-      if(line.substr(line.size()-id.size(), line.size())==id and linesSkip==0)
+      size=line.size();
+      line2=line.substr(size-idSize, size);
+
+      if(found and line.substr(size-idSize, size)!=id) break;
+      else if(line2==id and linesSkip==0)
 	{
-	  line=line.substr(1, line.size()-id.size()-1);
-	  // cout<<line<<'\n';
+	  line=line.substr(1, size-idSize-1);
 	  LineSplit(line, v);
+	  // Add vector to map
+	  print(v);
+	  v.clear();
+	  found=true;
 	}
-      else if (line.substr(line.size()-id.size(), line.size())==id) linesSkip--;
+      else if (line2==id) linesSkip--;
       else cout<<"END"<<'\n';
     }
 }
@@ -87,16 +98,10 @@ vector<string> GetMFMT(const string &element, const vector<string> &reactions, c
 
 void GetCrossSection()
 {
-
   const vector<string> reactions={"501", "502", "504", "515", "516", "517", "522", "534", "535", "536", "537", "538", "539", "540", "541", "542", "543"};
-
-
   const string MF="23";
   const string element="2600";
-
   const vector<string> reactions2=GetMFMT(element, reactions, MF);
-
-  const string steelTotalCrossSection="260023501";
   ifstream steel("./cross-sections/photoat-026_Fe_000.endf");
   streampos *begin=new streampos;
   *begin=0;
@@ -105,8 +110,9 @@ void GetCrossSection()
     {
       cout<<"FIND "<<reaction<<'\n';
       Read(steel, begin, reaction);
+      cout<<" "<<'\n';
     }
-  // Read(steel, begin, steelTotalCrossSection);
+
   cout<<*begin<<'\n';
   delete begin;
 }
