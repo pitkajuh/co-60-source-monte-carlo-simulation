c3de573f3f4922fd5dce83a3687f86fb5764071e
pitkajuh <github_pj@protonmail.com>
2024-12-14 15:08:59 +0200

    ok

--- ReadCrossSections.cpp
+++ ReadCrossSections.cpp
@@ -50,7 +50,7 @@ void LineSplit(string &line, vector<double> &v)
   else v.push_back(stod(line));
 }
 
-map<double, vector<double>> ReadENDF(ifstream &endf, streampos *&from, const string &id)
+map<double, vector<double>> ReadENDF(ifstream &endf, streampos &from, const string &id)
 {
   bool found=false;
   double energy;
@@ -62,18 +62,17 @@ map<double, vector<double>> ReadENDF(ifstream &endf, streampos *&from, const str
   vector<double> v;
   v.reserve(6);
   map<double, vector<double>> ENDFmap;
+  endf.seekg(from);
 
   while(getline(endf, line))
     {
       size=line.size();
       line2=line.substr(size-idSize, size);
-      // cout<<line.size()<<" "<<line<<'\n';
+
       if(found and line.substr(size-idSize, size)!=id) break;
       else if(line2==id and linesSkip==0)
 	{
-	  // cout<<line<<'\n';
 	  line=line.substr(1, size-idSize-1);
-	  // cout<<line<<" "<<line.size()<<'\n';
 	  LineSplit(line, v);
 	  energy=v[0];
 	  v.erase(v.begin()+0);
@@ -85,21 +84,6 @@ map<double, vector<double>> ReadENDF(ifstream &endf, streampos *&from, const str
       else if(line2==id) linesSkip--;
     }
   return ENDFmap;
-}
-
-map<double, vector<double>> Read(ifstream &endf, streampos *&from, const string &id)
-{
-  map<double, vector<double>> map;
-
-  if(from==nullptr)
-    {
-      from=new streampos;
-      *from=0;
-      map=ReadENDF(endf, from, id);
-      delete from;
-    }
-  else map=ReadENDF(endf, from, id);
-  return map;
 }
 
 void Split(string &line, vector<string> &v)
@@ -120,40 +104,37 @@ void Split(string &line, vector<string> &v)
     }
 }
 
-vector<string> GetReactions(ifstream &endf, streampos *&from)
+vector<string> GetReactions(ifstream &endf, streampos &from)
 {
-  bool found=false;
+  bool found=0;
   string line;
   string line2;
-  string line3;
   const string MFMT="MF/MT";
   const string warning="Warning";
-  int linesSkip=1;
   int at;
   vector<string> v;
   vector<string> result;
 
   while(getline(endf, line))
     {
-      line2=line.substr(2, MFMT.size());
 
       if(line.substr(2, warning.size())==warning) break;
-      else if(found and linesSkip==0 and line[1]!='=')
+      else if(found and line[1]!='=')
 	{
-	  line3=line.substr(2, line.size());
-	  Split(line3, v);
+	  line2=line.substr(2, line.size());
+	  Split(line2, v);
 	  at=distance(v[0].begin(), find(v[0].begin(), v[0].end(), '/'));
 	  result.push_back(v.back()+v[0].substr(0, at)+v[0].substr(at+1, v[0].size()));
 	  cout<<result.back()<<'\n';
 	  v.clear();
 	}
-      else if(found and linesSkip>0 and line[1]!='=') linesSkip--;
-      else if(line2==MFMT) found=true;
+      else if(line.substr(2, MFMT.size())==MFMT) found=true;
     }
+  from=endf.tellg();
   return result;
 }
 
-void ParseEndf(ifstream &endf, streampos *&from)
+void ParseEndf(ifstream &endf, streampos &from)
 {
   const vector<string> reactions=GetReactions(endf, from);
   map<double, vector<double>> map;
@@ -161,9 +142,11 @@ void ParseEndf(ifstream &endf, streampos *&from)
   for(const auto &reaction: reactions)
     {
       cout<<"FIND "<<reaction<<'\n';
-      map=Read(endf, from, reaction);
+      map=ReadENDF(endf, from, reaction);
       cout<<" "<<'\n';
     }
+  cout<<" "<<'\n';
+  from=0;
 }
 
 void GetCrossSection()
@@ -174,12 +157,10 @@ void GetCrossSection()
   ifstream titanium("./cross-sections/photoat-022_Ti_000.endf");
   ifstream iodine("./cross-sections/photoat-053_I_000.endf");
 
-  streampos *from=new streampos;
-  *from=0;
+  streampos from=0;
   ParseEndf(steel, from);
   ParseEndf(nitrogen, from);
   ParseEndf(sodium, from);
   ParseEndf(titanium, from);
   ParseEndf(iodine , from);
-  delete from;
 }
