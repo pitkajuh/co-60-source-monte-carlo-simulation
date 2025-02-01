9cce9ea832301b043339b1b2aba3c898acea37fa
pitkajuh <github_pj@protonmail.com>
2025-02-01 13:47:49 +0200

    ok

--- Material.h
+++ Material.h
@@ -6,7 +6,10 @@
 #include "PhotonAngularDistribution.h"
 #include "CentralDifference.h"
 
-const double GetMuValue(const double photonEnergy, map<double, pair<double, double>> &muMap, const double density)
+class Material
+{
+protected:
+  const double GetMuValue(const double photonEnergy, map<double, pair<double, double>> &muMap, const double density)
   {
     double energyPrevious=0;
     double energyCurrent=0;
@@ -23,9 +26,6 @@ const double GetMuValue(const double photonEnergy, map<double, pair<double, doub
     // Use linear interpolation to get the mass attenuation value corresponding to photonEnergy and turns it to linear attenuation coefficient. Returns the value in 1/m.
     return 100*density*(muMap[energyPrevious].first+(photonEnergy-energyPrevious)*(muMap[energyCurrent].first-muMap[energyPrevious].first)/(energyCurrent-energyPrevious));
   }
-
-class Material
-{
 public:
   Tape *endf=nullptr;
   PhotonAngularDistribution *incoherent=nullptr;
@@ -33,7 +33,7 @@ public:
   double density=0; // in g/cm3
   map<double, pair<double, double>> muMap;
   // CrossSections crossSections;
-  virtual const double GetMu(const double photonEnergy)=0;
+  const double GetMu(const double photonEnergy){return GetMuValue(photonEnergy, muMap, density);}
   virtual ~Material()
   {
     delete incoherent;
@@ -47,17 +47,15 @@ public:
   Steel(const double d, const string &endfTape)
   {
     this->endf=new Tape(endfTape);
-    incoherent=new IncoherentAngularDistribution(endf);
-    CentralDifference cd(incoherent, -1, 1, 1, 2e6, 100);
-    // coherent=new CoherentAngularDistribution(endf);
-    // CentralDifference cd1(coherent, -1, 1, 1, 1e4, 100);
+    // incoherent=new IncoherentAngularDistribution(endf);
+    // CentralDifference cd(incoherent, -1, 1, 1, 2e6, 100);
+    coherent=new CoherentAngularDistribution(endf);
+    CentralDifference cd1(coherent, -1, 1, 1, 1e4, 100);
     density=d;
     muMap=muMapSteel;
     // crossSections=steelCrossSections;
 
   }
-  const double GetMu(const double photonEnergy){return GetMuValue(photonEnergy, muMap, density);}
-
 };
 
 class Sodium: public Material
@@ -69,8 +67,6 @@ public:
     muMap=muMapSodium;
     // crossSections=sodiumCrossSections;
   }
-
-  const double GetMu(const double photonEnergy){return GetMuValue(photonEnergy, muMap, density);}
 };
 
 class Titanium: public Material
@@ -82,8 +78,6 @@ public:
     muMap=muMapTitanium;
     // crossSections=titaniumCrossSections;
   }
-
-  const double GetMu(const double photonEnergy){return GetMuValue(photonEnergy, muMap, density);}
 };
 
 class Nitrogen: public Material
@@ -95,8 +89,6 @@ public:
     muMap=muMapNitrogen;
     // crossSections=nitrogenCrossSections;
   }
-
-  const double GetMu(const double photonEnergy){return GetMuValue(photonEnergy, muMap, density);}
 };
 
 #endif
